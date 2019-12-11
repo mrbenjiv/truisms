7ad94f676a5e774374c8c95dfda9ded0efa17adc
mrbenjiv <58789828+mrbenjiv@users.noreply.github.com>
2019-12-11 12:11:20 -0800

    gh-pages

---
+++ precache-manifest.8654cad42e1aa329f2ea3d9404e2112b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86efe9f2aea866ead6e28e16719460d7",
+    "url": "/truisms/index.html"
+  },
+  {
+    "revision": "7ec16a4d930ae9ec7691",
+    "url": "/truisms/static/css/main.33785743.chunk.css"
+  },
+  {
+    "revision": "a1b25d940a096c1fb4cb",
+    "url": "/truisms/static/js/2.6cad17a5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/truisms/static/js/2.6cad17a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ec16a4d930ae9ec7691",
+    "url": "/truisms/static/js/main.e5784624.chunk.js"
+  },
+  {
+    "revision": "d8682bb1f32c17c9d989",
+    "url": "/truisms/static/js/runtime-main.aabf6c7f.js"
+  }
+]);
